867f3ecbb2e597a0a7e7102deb70e05ef96028cf
Shigma <shigma10826@gmail.com>
2021-12-08 14:09:55 +0800

    feat: using puppeteer

--- src/index.ts
+++ src/index.ts
@@ -14,22 +14,22 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+export const using = ['puppeteer']
+
 export function apply(ctx: Context) {
-  ctx.with(['puppeteer'], () => {
-    ctx.command('tex <code:rawtext>', 'TeX 渲染', { authority: 2 })
-      .usage('渲染器由 https://www.zhihu.com/equation 提供。')
-      .action(async (_, tex) => {
-        if (!tex) return '请输入要渲染的 LaTeX 代码。'
-        return ctx.puppeteer.render(null, async (page, next) => {
-          await page.goto('https://www.zhihu.com/equation?tex=' + escape(tex))
-          const svg = await page.$('svg')
-          const inner: string = await svg.evaluate((node: SVGElement) => {
-            node.style.padding = '0.25rem 0.375rem'
-            return node.innerHTML
-          })
-          const text = inner.match(/>([^<]+)<\/text>/)
-          return text ? text[1] : next(svg)
+  ctx.command('tex <code:rawtext>', 'TeX 渲染', { authority: 2 })
+    .usage('渲染器由 https://www.zhihu.com/equation 提供。')
+    .action(async (_, tex) => {
+      if (!tex) return '请输入要渲染的 LaTeX 代码。'
+      return ctx.puppeteer.render(null, async (page, next) => {
+        await page.goto('https://www.zhihu.com/equation?tex=' + escape(tex))
+        const svg = await page.$('svg')
+        const inner: string = await svg.evaluate((node: SVGElement) => {
+          node.style.padding = '0.25rem 0.375rem'
+          return node.innerHTML
         })
+        const text = inner.match(/>([^<]+)<\/text>/)
+        return text ? text[1] : next(svg)
       })
-  })
+    })
 }
